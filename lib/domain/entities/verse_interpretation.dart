6447b64f22107e8a76ea81079bf6481880fdcda0
syaoki09 <syaokifaradisa09@gmail.com>
2022-10-20 19:50:08 +0700

    Perbaikan nama Variabel Pada Entities Tafsir Ayat

--- lib/domain/entities/verse_interpretation.dart
+++ lib/domain/entities/verse_interpretation.dart
@@ -3,13 +3,13 @@ import 'package:equatable/equatable.dart';
 class VerseInterpretation extends Equatable{
   final int id;
   final int surahNumber;
-  final int number;
+  final int verseNumber;
   final String interpretation;
 
   const VerseInterpretation({
     required this.id,
     required this.surahNumber,
-    required this.number,
+    required this.verseNumber,
     required this.interpretation
   });
 
@@ -17,7 +17,7 @@ class VerseInterpretation extends Equatable{
   List<Object?> get props => [
     id,
     surahNumber,
-    number,
+    verseNumber,
     interpretation,
   ];
 }
